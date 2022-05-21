3913fd7f43f2a30726fbf27786dd74392d55495d
Devan Firmansyah Ramadhan <devanramadhan92@gmail.com>
2022-05-22 02:00:01 +0700

    [UPDATE] config deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import {defineConfig} from 'vite';
+import netlify from '@netlify/hydrogen-platform/plugin';
 import hydrogen from '@shopify/hydrogen/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hydrogen()],
+  plugins: [hydrogen(), netlify()],
   optimizeDeps: {include: ['@headlessui/react']},
   test: {
     globals: true,
