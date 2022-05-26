013cf9ab0677b71313b37d2956e0ee2941abb46a
Devan Firmansyah Ramadhan <devanramadhan92@gmail.com>
2022-05-27 02:18:53 +0700

    [UPDATE] rollback setting deploy vercel

--- serverless-entry.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-import '@shopify/hydrogen/dist/node/utilities/web-api-polyfill';
-import handleRequest from './src/App.server';
-// @ts-ignore
-import indexTemplate from './dist/client/index.html?raw';
-
-export default function (req, res) {
-  handleRequest(req, {streamableResponse: res, indexTemplate});
-}
