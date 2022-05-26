86b4a09b4e7417b85e1889dea77a3faf9678ff1a
Devan Firmansyah Ramadhan <devanramadhan92@gmail.com>
2022-05-26 15:05:33 +0700

    [UPDATE] header client

--- src/components/Header.client.jsx
+++ src/components/Header.client.jsx
@@ -29,6 +29,10 @@ export default function Header({collections, storeName}) {
 
   const {totalQuantity} = useCart();
 
+  useEffect(() => {
+    console.log(JSON.stringify(collections));
+  }, []);
+
   return (
     // <header className="h-20 lg:h-32" role="banner">
     //   <div
@@ -144,238 +148,19 @@ export default function Header({collections, storeName}) {
             </li>
             <li>
               <a href="shop.html" className="nav__item">
-                Shop
+                SHOP
               </a>
-              {/* MegaMenu */}
-              <div className="nav__mega-menu">
-                {/* Column */}
-                <div className="mega-menu__standard-column">
-                  {/* Column title */}
-                  <div className="standard-column__title">Shop</div>
-                  {/* End column title */}
-                  {/* Column nav */}
-                  <ul className="standard-column__nav">
-                    <li>
-                      <a href="shop.html">Grid 3 Column</a>
-                    </li>
-                    <li>
-                      <a href="shop-2.html">
-                        Grid 4 Column{' '}
-                        <span className="nav__item-tag nav__item-tag--default">
-                          default
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="shop-3.html">Listing</a>
-                    </li>
-                    <li>
-                      <a href="shop-4.html">Collection</a>
-                    </li>
-                    <li>
-                      <a href="shop-5.html">Metro</a>
-                    </li>
-                    <li>
-                      <a href="shop-6.html">
-                        Categories{' '}
-                        <span className="nav__item-tag nav__item-tag--hot">
-                          hot
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="shop-7.html">With Sidebar 1</a>
-                    </li>
-                    <li>
-                      <a href="shop-8.html">With Sidebar 2</a>
-                    </li>
-                    <li>
-                      <a href="shop-9.html">Instagram</a>
-                    </li>
-                    <li>
-                      <a href="shop-10.html">Vertical Menu</a>
-                    </li>
-                  </ul>
-                  {/* End column nav */}
-                </div>
-                {/* End column */}
-                {/* Column */}
-                <div className="mega-menu__standard-column">
-                  {/* Column title */}
-                  <div className="standard-column__title">Product layouts</div>
-                  {/* End column title */}
-                  {/* Column nav */}
-                  <ul className="standard-column__nav">
-                    <li>
-                      <a href="product.html">Default</a>
-                    </li>
-                    <li>
-                      <a href="product-layout-2.html">
-                        Sticky Info{' '}
-                        <span className="nav__item-tag nav__item-tag--hot">
-                          hot
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-layout-3.html">With Sidebar</a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">Color Background</a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">Vertical Menu</a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">Width 1170</a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">
-                        Sticky Info 2{' '}
-                        <span className="nav__item-tag nav__item-tag--new">
-                          new
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">
-                        Sticky Info 3{' '}
-                        <span className="nav__item-tag nav__item-tag--new">
-                          new
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">
-                        Grid Gallery (4 images){' '}
-                        <span className="nav__item-tag nav__item-tag--new">
-                          new
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">
-                        Grid Gallery (5 images){' '}
-                        <span className="nav__item-tag nav__item-tag--new">
-                          new
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-layout-4.html">
-                        Sticky Tabs Accordion{' '}
-                        <span className="nav__item-tag nav__item-tag--new">
-                          new
-                        </span>
-                      </a>
-                    </li>
-                  </ul>
-                  {/* End column nav */}
-                </div>
-                {/* End column */}
-                {/* Column */}
-                <div className="mega-menu__standard-column">
-                  {/* Column title */}
-                  <div className="standard-column__title">Product types</div>
-                  {/* End column title */}
-                  {/* Column nav */}
-                  <ul className="standard-column__nav">
-                    <li>
-                      <a href="product-type-1.html">Simple</a>
-                    </li>
-                    <li>
-                      <a href="product-type-2.html">Various</a>
-                    </li>
-                    <li>
-                      <a href="product-type-3.html">Colors Swatches</a>
-                    </li>
-                    <li>
-                      <a href="product-type-4.html">Images Swatches</a>
-                    </li>
-                    <li>
-                      <a href="product-type-5.html">Groupped</a>
-                    </li>
-                    <li>
-                      <a href="product-type-6.html">Affiliate</a>
-                    </li>
-                    <li>
-                      <a href="product-type-7.html">Featured Video</a>
-                    </li>
-                    <li>
-                      <a href="product-type-8.html">Guarantee Safe Checkout</a>
-                    </li>
-                    <li>
-                      <a href="product-type-9.html">Countdown Timer</a>
-                    </li>
-                    <li>
-                      <a href="product-type-10.html">
-                        Pre-orders{' '}
-                        <span className="nav__item-tag nav__item-tag--hot">
-                          hot
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-type-1.html">On Sale</a>
-                    </li>
-                    <li>
-                      <a href="product-type-1.html">Out of Stock</a>
-                    </li>
-                    <li>
-                      <a href="product-type-10.html">
-                        With Button Paypal{' '}
-                        <span className="nav__item-tag nav__item-tag--hot">
-                          hot
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-type-1.html">Bundle</a>
-                    </li>
-                  </ul>
-                  {/* End column nav */}
-                </div>
-                {/* End column */}
-                {/* Column */}
-                <div className="mega-menu__standard-column">
-                  {/* Column title */}
-                  <div className="standard-column__title">Others</div>
-                  {/* End column title */}
-                  {/* Column nav */}
-                  <ul className="standard-column__nav">
-                    <li>
-                      <a href="cart.html">Shopping Cart</a>
-                    </li>
-                    <li>
-                      <a href="checkout.html">Checkout</a>
-                    </li>
-                    <li>
-                      <a href="order-complete.html">Order Complete</a>
-                    </li>
-                    <li>
-                      <a href="wishlist.html">Wishlist</a>
-                    </li>
-                    <li>
-                      <a href="cart.html">Order Tracking</a>
-                    </li>
-                  </ul>
-                  {/* End column nav */}
-                </div>
-                {/* End column */}
-                {/* Banner */}
-                <div className="mega-menu__banner">
-                  <img
-                    alt="Image"
-                    data-sizes="auto"
-                    data-srcset="assets/images/default/megamenu_promo.jpg 362w,
-              assets/images/default/megamenu_promo.jpg 724w"
-                    src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-                    className="lazyload"
-                  />
-                </div>
-                {/* End banner */}
-              </div>
-              {/* End MegaMenu */}
+              <ul className="nav__dropdown-menu">
+                <li>
+                  <Link to="/shop">Shop All</Link>
+                </li>
+                <li>
+                  <a href="blog-with-sidebar.html">Blog with sidebar</a>
+                </li>
+                <li>
+                  <a href="post.html">Blog post</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="about.html" className="nav__item">
